0b061910828eda1dc581de733d3404746805ee8d
David Brownell <db@davidbrownell.com>
2024-04-26 15:09:48 -0400

    Minor updates
    - Dynamically calculating ProjectTypes
    - Updated CI workflow dependencies to 0.15.4
    - Template changes:
        * Added DEVELOPMENT.md (and removed content from README.md)
        * Displaying info during activation when environment was bootstrapped with 'package'
        * Updated CI workflow dependencies to 0.15.4

--- src/PythonProjectBootstrapper/EntryPoint.py
+++ src/PythonProjectBootstrapper/EntryPoint.py
@@ -20,6 +20,8 @@ from typer.core import TyperGroup  # type: ignore [import-untyped]
 from cookiecutter.main import cookiecutter
 from dbrownell_Common.ContextlibEx import ExitStack
 from dbrownell_Common import PathEx
+from dbrownell_Common.Streams.DoneManager import DoneManager
+
 from PythonProjectBootstrapper import __version__
 from PythonProjectBootstrapper.ProjectGenerationUtils import (
     CopyToOutputDir,
@@ -51,10 +53,31 @@ app = typer.Typer(
 
 
 # ----------------------------------------------------------------------
-class ProjectType(str, Enum):
-    """Defines the different types of projects that can be specified on the command line."""
+# Dynamically create the ProjectType enumeration based on the directories found in this dir.
 
-    package = "package"
+
+def _CreateProjectType():
+    project_types: list[str] = []
+
+    if getattr(sys, "frozen", False):
+        template_dir = Path(sys.executable).parent / "lib" / "PythonProjectBootstrapper"
+    else:
+        template_dir = Path(__file__).parent
+
+    PathEx.EnsureDir(template_dir)
+
+    for file_item in template_dir.iterdir():
+        if not file_item.is_dir():
+            continue
+
+        project_types.append(file_item.name)
+
+    assert project_types
+    return Enum("ProjectType", {project_type: project_type for project_type in project_types})
+
+
+ProjectType = _CreateProjectType()
+del _CreateProjectType
 
 
 # ----------------------------------------------------------------------
@@ -88,6 +111,11 @@ _version_option = typer.Option(
     is_eager=True,
 )
 
+_skip_prompts_option = typer.Option(
+    "--skip-prompts",
+    help="Do not display prompts after generating content.",
+)
+
 
 # ----------------------------------------------------------------------
 # The cookiecutter project dir must be accessed in different ways depending on whether the code is:
@@ -113,6 +141,7 @@ if getattr(sys, "frozen", False):
         configuration_filename: Annotated[Optional[Path], _configuration_filename_option] = None,
         replay: Annotated[bool, _replay_option] = False,
         yes: Annotated[bool, _yes_option] = False,
+        skip_prompts: Annotated[bool, _skip_prompts_option] = False,
         version: Annotated[bool, _version_option] = False,  # pylint: disable=unused-argument
     ) -> None:
         if output_dir.is_file():
@@ -128,6 +157,7 @@ if getattr(sys, "frozen", False):
             configuration_filename,
             replay=replay,
             yes=yes,
+            skip_prompts=skip_prompts,
         )
 
     # ----------------------------------------------------------------------
@@ -145,6 +175,7 @@ else:
         configuration_filename: Annotated[Optional[Path], _configuration_filename_option] = None,
         replay: Annotated[bool, _replay_option] = False,
         yes: Annotated[bool, _yes_option] = False,
+        skip_prompts: Annotated[bool, _skip_prompts_option] = False,
         version: Annotated[bool, _version_option] = False,  # pylint: disable=unused-argument
     ) -> None:
         _ExecuteOutputDir(
@@ -153,6 +184,7 @@ else:
             configuration_filename,
             replay=replay,
             yes=yes,
+            skip_prompts=skip_prompts,
         )
 
 
@@ -169,6 +201,7 @@ def _ExecuteOutputDir(
     *,
     replay: bool,
     yes: bool,
+    skip_prompts: bool,
 ) -> None:
     if not (output_dir / ".git").is_dir():
         raise Exception(f"{output_dir} is not a git repository.")
@@ -190,18 +223,21 @@ def _ExecuteOutputDir(
                 if execute_func(project_dir, tmp_dir, yes=yes) is False:
                     return
 
-    # generate project in temporary directory so we can avoid overwriting files without user approval
-    cookiecutter(
-        str(project_dir),
-        output_dir=str(tmp_dir),
-        config_file=str(configuration_filename) if configuration_filename is not None else None,
-        replay=replay,
-        overwrite_if_exists=True,
-        accept_hooks=True,
-    )
+    with DoneManager.Create(sys.stdout, "\nGenerating content..."):
+        # generate project in temporary directory so we can avoid overwriting files without user approval
+        cookiecutter(
+            str(project_dir),
+            output_dir=str(tmp_dir),
+            config_file=str(configuration_filename) if configuration_filename is not None else None,
+            replay=replay,
+            overwrite_if_exists=True,
+            accept_hooks=True,
+        )
 
     CopyToOutputDir(src_dir=tmp_dir, dest_dir=output_dir)
-    DisplayPrompt(output_dir=output_dir)
+
+    if not skip_prompts:
+        DisplayPrompt(output_dir=output_dir)
 
 
 if __name__ == "__main__":
